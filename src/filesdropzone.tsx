ac5eb371838cb777661d218fb59de10c8fb65b2a
Alsprite <henri.mykra@esedulainen.fi>
2024-02-06 11:30:29 +0200

    css kunnol

--- src/filesdropzone.tsx
+++ src/filesdropzone.tsx
@@ -1,32 +1,49 @@
 /* eslint-disable */
 import React from 'react';
-import Button from '@material-ui/core/Button'
+import { Typography, Box, Button } from '@material-ui/core/'
 import { makeStyles } from '@mui/styles'
 
 export interface dragDropText {
-
+  text?: string;
 }
 
 export interface orText {
-
+ text?: string;
 }
 
 export interface selectFileButtonLabel {
-  
+  text?: string;
 }
 
 const useStyles = makeStyles({
   box: {
-    display: 'flex'
+    display: 'flex',
+    maxWidth: "25%",
+    minHeight: "15vh",
+    justifyContent: 'center',
+    alignItems: 'center',
+    border: 'solid 3px',
+    borderColor: 'black',
+    whiteSpace: 'pre-wrap',
+    flexDirection: 'column',
+    padding: '10px',
+    margin: '0 auto'
+  },
+  text: {
+    display: 'inline'
   }
 })
 
 const FilesDropZone = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>test</h1>
-      <Button variant="contained"></Button>
-    </div>
+    <Box className={classes.box}>
+      <Typography className={classes.text}>DRAG-DROP HERE</Typography>
+      <br />
+      <Typography className={classes.text}>OR</Typography>
+      <br />
+      <Button variant="contained" className={classes.text}>SELECT FILE</Button>
+    </Box>
   );
 };
 
