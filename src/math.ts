e84443a54829c1f6a7016985b87465c8be950a84
Alsprite <henri.mykra@esedulainen.fi>
2024-02-15 12:29:25 +0200

    ts works with ts-node

--- src/math.ts
+++ src/math.ts
@@ -1,39 +1,41 @@
 const math = require('mathjs');
 
-const data = {
-  some: {
-    path: {
-      to: {
-        value: 42
-      }
-    }
-  }
-}
-
 function withScope() {
-  const scope = { x:3 }
-  math.evaluate('x', scope)
-  return scope
+    const scope = {
+        x: 3,
+        y: 4,
+        kissa: { koira: 'ok', age: 6}
+    }
+    const value = math.evaluate('x * y', scope);
+    console.log(value);
+
+    math.get = function (key: string, fallback: any) {
+        const value = math.evaluate(`scope.${key}`, { scope });
+        return value !== undefined ? value : fallback;
+    };
+
+    math.gt = function (key1: string, key2: string, fallback: boolean = false) {
+        const value1 = math.evaluate(`scope.${key1}`, { scope });
+        const value2 = math.evaluate(`scope.${key2}`, { scope });
+        return value1 !== undefined && value2 !== undefined ? value1 > value2 : fallback;
+    };
+
+    math.lt = function (key1: string, key2: string, fallback: boolean = false) {
+        const value1 = math.evaluate(`scope.${key1}`, { scope });
+        const value2 = math.evaluate(`scope.${key2}`, { scope });
+        return value1 !== undefined && value2 !== undefined ? value1 < value2 : fallback;
+    };
+
+    console.log(math.get('x', 'Fallback Value'));
+    console.log(math.gt('x', 'y', false));
+    console.log(math.lt('x', 'y', true));
+
+
+    math.evaluate("get('kissa.koira', 'janis')") // ok
+    math.evaluate("get('rotta.karhu', 'janis')") // janis
+    math.evaluate("gt('x', 'kissa.age', false)")
+
+    math.evaluate('kissa.lentokone == 0')
 }
 
-math.get = function (key: string, fallback: any) {
-  const value = math.evaluate(`data.${key}`, { data });
-  return value !== undefined ? value : fallback;
-};
-
-math.gt = function (key1: string, key2: string, fallback: boolean = false) {
-  const value1 = math.evaluate(`data.${key1}`, { data });
-  const value2 = math.evaluate(`data.${key2}`, { data });
-  return value1 !== undefined && value2 !== undefined ? value1 > value2 : fallback;
-};
-
-math.lt = function (key1: string, key2: string, fallback: boolean = false) {
-  const value1 = math.evaluate(`data.${key1}`, { data });
-  const value2 = math.evaluate(`data.${key2}`, { data });
-  return value1 !== undefined && value2 !== undefined ? value1 < value2 : fallback;
-};
-
-console.log(math.get('some.path.to.value', 'Fallback Value'));
-console.log(math.gt('some.path.to.value', 'some.path.to.otherValue', false));
-console.log(math.lt('some.path.to.value', 'some.path.to.otherValue', true));
 withScope();
